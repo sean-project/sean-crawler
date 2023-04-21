2a70a257c6fca9e867023666bbe22a95393882bd
Allen Zhang <wr_zhang25@163.com>
2023-04-21 11:23:18 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer';
+import {sleep} from "./helpers";
+import * as mongodb from 'mongodb'
+import starNames from './starNames.json'
+
+const mongourl = "mongodb://192.168.0.107:27017/sean";
+(async () => {
+    const db = await mongodb.MongoClient.connect(mongourl)
+    const dbo = db.db("sean")
+
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+
+    page.on('response', async response => {
+        if (response.url().includes('api/container/getIndex?uid')){
+            try {
+                await dbo.collection("star").insertMany([await response.json()])
+            } catch (e) {
+
+            }
+        }
+    })
+    for (let i = 0; i < starNames.length; i++) {
+        await page.goto('https://m.weibo.cn/search?containerid=231583');
+        // Set screen size
+        await page.setViewport({width: 375, height: 1024});
+        await page.waitForSelector("input[type=search]",{timeout:3000})
+        // Type into search box
+        await page.type("input[type=search]", starNames[i]);
+        // 回车
+        await page.keyboard.press('Enter');
+        // Wait and click on first result
+        const searchResultSelector = 'div.card.m-panel.card28.m-avatar-box > div > div > div > div.m-img-box';
+        await page.waitForSelector(searchResultSelector,{timeout:3000});
+        await page.click(searchResultSelector);
+
+        // const response = await page.waitForResponse((res)=>res.url().includes('api/container/getIndex?uid'))
+        // await dbo.collection("star").insertMany([await response.json()])
+        await sleep(6000)
+    }
+    // await browser.close();
+})();
